636e2e980245380aa0bdf3fd0cc1bb4b9fe07cad
Brett L. Schuchert <schuchert@yahoo.com>
2011-10-07 22:06:49 -0500

    Updated to check for NULL

--- CommonMemory.h
+++ CommonMemory.h
@@ -5,10 +5,12 @@
 #ifdef CPPUTEST_USE_MEM_LEAK_DETECTION
 #include <CppUTest/TestHarness_c.h>
 #define acquire(s) cpputest_malloc_location((s), __FILE__, __LINE__)
-#define release(s) cpputest_free_location((s), __FILE__, __LINE__)
+#define release(s) if(s != NULL) cpputest_free_location((s), __FILE__, __LINE__)
 #else
+#include <memory.h>
+#include <stddef.h>
 #define acquire(s) malloc((s))
-#define release(s) free((s))
+#define release(s) if(s != NULL) free(s)
 #endif
 
 #include <stdlib.h>
